e42ffc1a7361baa0cf39f976901f502b24cdad8e
Melissa Belfer <belfer.rose@gmail.com>
2018-09-21 15:20:27 -0400

    Updates

---
+++ script/navbar.js
@@ -0,0 +1,24 @@
+
+var toggle_btn = document.getElementById("btn-toggle");
+var navSection = document.getElementById("navbarSupportedContent");
+var open = false;
+toggle_btn.onclick = function(){
+    // if(open){
+    //     //add right back
+    //     console.log("yeet");
+    //     open = false;
+
+    // }else{
+    //     //remove right
+    //     console.log("sup");
+    //     open = true;
+    //     navSection.removeClass("right");
+    // }
+
+    // if(navSection.className === "right"){
+    //     navSection.className += " responsive";
+    // }else{
+    //     navSection.className = "right";
+    // }
+};
+
